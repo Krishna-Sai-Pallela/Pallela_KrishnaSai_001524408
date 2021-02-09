0ecc7d840d12518bf14abf262fe3c18c176412f7
Krishna-Sai-Pallela <78462548+Krishna-Sai-Pallela@users.noreply.github.com>
2021-02-08 22:25:39 -0500

    first commit

---
+++ Assignment_1/src/Business/DLicenseInfo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author krish
+ */
+public class DLicenseInfo {
+    
+    private String licenseNumber;
+    private String dateOfIssue;
+    private String dateOfExpiry;
+    private String bloodType;
+//    private String picture;
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getDateOfExpiry() {
+        return dateOfExpiry;
+    }
+
+    public void setDateOfExpiry(String dateOfExpiry) {
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+    
+    
+}
---
+++ Assignment_1/src/Business/Demographic.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author krish
+ */
+public class Demographic {
+    
+    private String firstName;
+    private String lastName;
+    private String phoneNubmer;
+    private String age;
+    private String dob; 
+    private String height;
+    private String weight;
+    private String SSN;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNubmer() {
+        return phoneNubmer;
+    }
+
+    public void setPhoneNubmer(String phoneNubmer) {
+        this.phoneNubmer = phoneNubmer;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+    
+    
+    
+}
