c30c420541fff6c578fa1384fe10b8871acdf2f1
Krishna-Sai-Pallela <78462548+Krishna-Sai-Pallela@users.noreply.github.com>
2021-03-21 23:58:05 -0400

    Update Transcript.java

--- brewinguniversitymodel/brewinguniversitymodel/src/main/java/info5100/university/brewingUniversityModel/Persona/Transcript.java
+++ brewinguniversitymodel/brewinguniversitymodel/src/main/java/info5100/university/brewingUniversityModel/Persona/Transcript.java
@@ -34,6 +34,8 @@ public class Transcript {
         return currentcourseload;
     }
     
+    //Calculate GPA 
+    
     public CourseLoad getCurrentCourseLoad(){
         
         return currentcourseload;
